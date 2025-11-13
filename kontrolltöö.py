11aaac193a9424f10d54741f5cf704f12973995b
opilane <opilane@HP-CZC2349HTQ>
2025-11-13 11:22:04 +0200

    correct

--- kontrolltöö.py
+++ kontrolltöö.py
@@ -16,6 +16,7 @@
 #             for l in range(num):
 #                 print(" __\t", end=' ')
 #             print('')
+#             break
 #         else:
 #             print("Viga: Sisestatud arv ei ole vahemikus 1 kuni 9.")
 #     except :
@@ -26,16 +27,16 @@
 # import random
 
 # summa = 0
-# kesk_kuus = 0
+# kuus_summa = 0
 # for aasta in range(12):
 #     kulud = random.randint(5, 8)
-#     for kuu in range(kulud): 
+#     for kuus in range(kulud): 
 #         summa += round(random.randint(50, 100) * 1.11, 2) 
 #     summa = round(summa, 2)
 #     print('kuus oli: ' + str(summa) + '€')
-#     kesk_kuus += summa
+#     kuus_summa += summa
 #     summa = 0
-# print('keskmised kulud aastas on: ' + str(round(kesk_kuus/12, 2)) + '€')
+# print('keskmised kulud aastas on: ' + str(round(kuus_summa/12, 2)) + '€')
 
 # Ülesanne 3
 
@@ -52,17 +53,32 @@
 
 # Ülesanne 4
 # import random
+# while True:
+#     try:
+#         a = int(input("Sisesta täisarv vahemikus 1 kuni 10: "))
+#         b = int(input("Sisesta täisarv vahemikus 1 kuni 10: "))
 
-# a = int(input("Sisesta täisarv vahemikus 1 kuni 10: "))
-# b = int(input("Sisesta täisarv vahemikus 1 kuni 10: "))
-
-# for i in range(b):
-#     for u in range(a):
-#         print(random.randint(10, 100), end='\t')
-#     print('')
+#         for i in range(b):
+#             for u in range(a):
+#                 print(random.randint(10, 100), end='\t')
+#             print('')
+#             break
+#     except:
+#         print("Valed andmed")
         
 # Ülesanne 5
-import random
-
-tootaajad = int(input("Sisesta töötajate arv: "))
-kas_pensionaar = random.randint(0,1)
+# import random
+# while True:
+#     kui_palju = 0
+#     a = random.randint(1400, 1700)
+#     try:
+#         tootaajad = int(input("Sisesta töötajate arv: "))
+#         for i in range(tootaajad):
+#             kas_pensionaar = random.randint(0,1)
+#             palk = random.randint(1200, 3000)
+#             if kas_pensionaar == 1 and palk>a:
+#                 kui_palju += 1
+#         print(f'Kokku on {kui_palju} töötajat, kes on pensionär ja kelle palk on suurem kui {a}€')
+#         break
+#     except:
+#         print("Viga: Palun sisesta kehtiv täisarv.")
